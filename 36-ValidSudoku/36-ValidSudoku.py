1c5c7a98ccfca68f685c0e4bc3094f38d35c5f06
Alle Likhitha <35693319+alle-likhitha@users.noreply.github.com>
2025-03-17 23:10:45 -0700

    File updated at 3/17/2025, 11:10:45 PM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,30 +1,13 @@
+// Last updated: 3/17/2025, 11:10:45 PM
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        temp1 = {}
-        temp2 = {}
-        temp3 = defaultdict(set)
+        res = []
         for i in range(9):
             for j in range(9):
-                if board[i][j] not in temp1 and board[i][j] != '.':
-                    temp1[board[i][j]] = 1
-                elif board[i][j] != '.':
-                    return False
-                if board[j][i] not in temp2 and board[j][i] != '.':
-                    temp2[board[j][i]] = 1
-                elif board[j][i] != '.':
-                    return False
-                # print(temp1, temp2)
-            temp1 = {}
-            temp2 = {}
-        print("1, 2, Passsss")
-        # temp1 = []
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == ".":
-                    continue
+                elem = board[i][j]
+                if elem != '.':
+                    res += [(i, elem), (elem, j), (i // 3, j//3, elem)]
+        print(set(res))
+        return len(res) == len(set(res))
 
-                if board[i][j] in temp3[(i//3,j//3)]:
-                    return False
-                temp3[(i//3, j//3)].add(board[i][j])
         
-        return True
