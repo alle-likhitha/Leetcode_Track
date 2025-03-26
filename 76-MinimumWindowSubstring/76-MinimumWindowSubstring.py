c5cdd565f41d9ea808cf6a07f60209e9eeb4ea41
Alle Likhitha <35693319+alle-likhitha@users.noreply.github.com>
2025-03-25 19:54:14 -0700

    File updated at 3/25/2025, 7:54:14 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,39 +1,59 @@
-# Last updated: 3/25/2025, 7:37:43 PM
-from collections import Counter
-
+# Last updated: 3/25/2025, 7:54:14 PM
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t or len(s) < len(t):
+        if len(t) > len(s):
             return ""
-
-        t_dict = Counter(t)  # Frequency of characters in t
-        window = {}
-        
-        left = 0
-        min_len = float("inf")
-        min_substr = ""
-        
-        required = len(t_dict)  # Number of unique chars in t that must match
-        formed = 0  # Number of unique chars in current window matching the required frequency
-
-        for right in range(len(s)):
-            char = s[right]
-            window[char] = window.get(char, 0) + 1
-
-            if char in t_dict and window[char] == t_dict[char]:
-                formed += 1
+        if s == t:
+            return t
+        in_st_dict = {}
+        t_dict = {}
+        # res = []
+        result = ""
+        temp_to_check = True
+        i = 0
+        j = 0
+        for tt in range(len(t)):
+            t_dict[t[tt]] = t_dict.get(t[tt], 0) + 1
             
-            # Try shrinking the window
-            while left <= right and formed == required:
-                if (right - left + 1) < min_len:
-                    min_len = right - left + 1
-                    min_substr = s[left:right + 1]
+       
+        while(j < len(s) and i < len(s)):
+            # print(i,j)
+        # for j in range(len(t),len(s)):
+            temp_to_check = True
+            if s[j] in t:
+                in_st_dict[s[j]] = in_st_dict.get(s[j], 0) + 1
+            # temp_to_check = True
+            
+            for key, val in t_dict.items():
+                if key not in in_st_dict or val > in_st_dict[key]:
+                    temp_to_check = False
+                    break
+
+            while(temp_to_check):
+                while s[i] not in t:
+                    i += 1
+            
+                if result == "":
+                    result = s[i:j+1]
+                elif len(result) > j - i + 1:
+                    result = s[i:j+1]
+                in_st_dict[s[i]] -= 1
+
+                if in_st_dict[s[i]] == 0:
+                    in_st_dict.pop(s[i])
+                i += 1
                 
-                # Remove leftmost character from window
-                window[s[left]] -= 1
-                if s[left] in t_dict and window[s[left]] < t_dict[s[left]]:
-                    formed -= 1
+                for key, val in t_dict.items():
+                    # print(t_dict,'items in t')
+                    # print(in_st_dict, 'items in new st dict')
+                    if key not in in_st_dict or val > in_st_dict[key]:
+                        temp_to_check = False
+                        break
+                    
 
-                left += 1  # Move the left pointer
-
-        return min_substr
+            # print(i,j)
+            j += 1
+        # print(res)
+        # print(result)
+        return result
+        
