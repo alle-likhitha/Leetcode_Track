f570998e02fa24f306d57f4818d54ae85867aca0
Alle Likhitha <35693319+alle-likhitha@users.noreply.github.com>
2025-04-02 19:47:28 -0700

    File updated at 4/2/2025, 7:47:28 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
@@ -1,10 +1,15 @@
-# Last updated: 4/2/2025, 7:40:28 PM
+# Last updated: 4/2/2025, 7:47:28 PM
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         if len(cost) == 2:
             return min(cost)
-        stack = [cost[-1], cost[-2]]
+        temp1 = cost[-1]
+        temp2 = cost[-2]
+        res = min(cost[-1], cost[-2])
         for i in range(len(cost)-3,-1,-1):
-            stack.append(cost[i] + min(stack[-1], stack[-2]))
-            # print(stack)
-        return min(stack[-1], stack[-2])
+            res = min(temp1+ cost[i], temp2+cost[i])
+            temp1 = temp2
+            temp2 = res
+            # print(res)
+        # return min(stack[-1], stack[-2])
+        return min(res, temp1)
