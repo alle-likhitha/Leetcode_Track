ba47678aebc6f73062beb36b234a85a9ac6c56af
Alle Likhitha <35693319+alle-likhitha@users.noreply.github.com>
2025-04-02 17:40:54 -0700

    Create file

---
+++ 2874-MaximumValueofanOrderedTripletII/2874-MaximumValueofanOrderedTripletII.py
@@ -0,0 +1,19 @@
+# Last updated: 4/2/2025, 5:40:54 PM
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        right_max = [0] * len(nums)
+        right_max[-1] = nums[-1]
+        for i in range(len(nums) - 2, -1, -1):
+            right_max[i] = max(right_max[i+1], nums[i])
+        # print(right_max)
+        stack = []
+        res = 0
+
+        for i in range(len(nums) - 1):
+            while stack and stack[-1] <= nums[i]:
+                stack.pop()
+            stack.append(nums[i])
+            if stack:
+                res = max(res, ((stack[0] - stack[-1]) * right_max[i+1]))
+            # print(res)
+        return res
